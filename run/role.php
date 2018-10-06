ddb4748974944157701af4be8841de88b09b9409
Mathieu Durand <mathieu.durand@fr.ibm.com>
2018-10-06 23:35:34 +0200

    oubli

--- run/role.php
+++ run/role.php
@@ -39,7 +39,7 @@
         }
 		echo "<br>";
 		$message="http://run.planning-gn.fr/role.php?role=".$role;
-		echo "<img src=\"data:image/png;base64,".QRcode::png('$message')."></img>;
+		echo "<img src=\"data:image/png;base64,".QRcode::png('$message')."\"></img>;
         echo '<br>objectifs<br>';
         $sql="select id,nom,relation from objectif where gn='$gn' and role='$role'";
         $result=mysqli_query($db,$sql);

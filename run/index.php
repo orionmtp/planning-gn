e922e3d6b05ed1cfc17c05d3d9dc5f1aabcc1ce3
Mathieu Durand <mathieu.durand@fr.ibm.com>
2018-10-06 21:36:27 +0200

    ajout de la page d'accueil du run

---
+++ run/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<html><header></header><body><center>";
+echo "ce site n'est accessible que pour les orgas, lors d'une session de jeu<br>";
+echo "voyez avec votre responsable applications<br>";
+echo "<a href=http://scenar.planning-gn.fr>interface scenariste</a><br>";
+echo '</center></body></html>';
+?>

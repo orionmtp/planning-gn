96c375cbbcdd556a9df3af2f555536bbb2a50cb2
Mathieu Durand <mathieu.durand@fr.ibm.com>
2018-10-06 23:48:44 +0200

    un fichier de test basique

---
+++ run/test.php
@@ -0,0 +1,5 @@
+<?php 
+
+    // how to use image from example 001 
+    echo '<img src="test_png.php" />';
+?>
---
+++ run/test_png.php
@@ -0,0 +1,8 @@
+<?php 
+
+    include('../lib/full/qrlib.php'); 
+     
+    // outputs image directly into browser, as PNG stream 
+    QRcode::png('on se l\'demande...');
+	
+?>

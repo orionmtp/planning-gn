d4927bb975e1a79b69afc832fbce83a0bbd3e1dc
Mathieu Durand <mathieu.durand@fr.ibm.com>
2018-10-07 10:29:37 +0200

    parce que je suis tjrs un boulet et que j'ai oublié l'info

--- run/create_png.php
+++ run/create_png.php
@@ -1,5 +1,5 @@
 <?php 
-    include('phpqrcode/qrlib.php'); 
+    include('../common/phpqrcode/qrlib.php'); 
     $text=$_GET['text'];
     // outputs image directly into browser, as PNG stream
     QRcode::png($text);

0cd8f5c8377ff50acefc2147b67eb3eaa31f383e
Mathieu Durand <mathieu.durand@fr.ibm.com>
2018-10-07 20:48:40 +0200

    correction insert GN

--- scenar/add-gn.php
+++ scenar/add-gn.php
@@ -11,7 +11,7 @@ else {
     if (isset($_POST['nom'])) {
        $nom=mysqli_real_escape_string ($db,$_POST['nom']);
         $date=date("Y-m-d H:i").":00";
-        $sql="insert into gn (nom,debut,fin,description,presentation,nb_pnj,nb_pj,paf_pnj,paf_pj,rue,ville,pays,CP,fedeGN,website,avant,scenario,running,delta,avance,serial,recur) values ('$nom','$date','$date','resume','presentation','0','0','0','0','adresse','ville','france','0000','','website','evenements precedents','scenario','0','0','0',NULL,'')";
+        $sql="insert into gn (nom,debut,fin,description,presentation,nb_pnj,nb_pj,paf_pnj,paf_pj,rue,ville,pays,CP,fedeGN,website,avant,scenario,running,delta,avance,serial,recur) values ('$nom','$date','$date','resume','presentation','0','0','0','0','adresse','ville','france','0000','0','website','evenements precedents','scenario','0','0','0',NULL,'')";
         mysqli_query($db,$sql)  or die(mysqli_error($db));
         $res=mysqli_insert_id($db);
         $sql="insert into admin (gn,login,admin,medical,role,confirmed) values ('$res','$id','1','0','admin','1')";

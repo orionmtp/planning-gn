7308db0baaf3c505ad1a9c6fe1aa26741dee214a
Mathieu Durand <mathieu.durand@fr.ibm.com>
2018-10-11 23:41:23 +0200

    avancée majeure sur la moulinette
    remplis le planning, mais ne gere pas vraiment correctement les reservations sur les evenements reccurents
    ni les reservations sur les evenements non validés

--- scenar/moulinette.php
+++ scenar/moulinette.php
@@ -25,14 +25,53 @@ else {
 		else     
 		{
 			echo "c'est en cours<br>";
-// on fait la liste des besoins et des heures de debut et fin correspondantes, classé par preparation et priorité
-			$sql="select listPNJ.id as besoin, listPNJ.role as role, listPNJ.login as joueur, (select count(style_pnj.role) from style_pnj inner join style_joueur on style_pnj.style=style_joueur.style where joueur=listPNJ.login and role=listPNJ.role)as stylecommun, (select count(style) from style_pnj where role=listPNJ.role) as stylerole, (select count(style) from style_joueur where joueur=listPNJ.login) as stylejoueur from (select listbesoin.id as id,listbesoin.role as role,inscription.login from inscription, (select besoin.id,besoin.role, debut-prepa as deb, debut+duree as fin, priorite as prio from event inner join besoin on besoin.event=event.id inner join role on besoin.role=role.id where event.gn='$gn' and role.pnj='1' order by debut-prepa, priorite) as listbesoin where pnj='1' and gn='$gn' and login not in (select planning.joueur from planning inner join inscription as truc on truc.login=planning.joueur where planning.gn='$gn' and ((debut>listbesoin.deb and debut<listbesoin.fin) or (fin>listbesoin.deb and fin<listbesoin.fin) or (debut<listbesoin.deb and fin>listbesoin.fin)))) as listPNJ order by stylerole desc, stylecommun desc,stylejoueur asc ";
-			$result=mysqli_query($db,$sql)  or die(mysqli_error($db));
-			if (mysqli_num_rows($result) > 0) {
-				while($row = mysqli_fetch_assoc($result)) {	
-					echo "besoin : ".$row['besoin']." role : ".$row['role']." joueur : ".$row['joueur']." role : ".$row['stylerole']." commun : ".$row['stylecommun']." PNJ : ".$row['stylejoueur']."<br>";
+			$sql1="select count(besoin.id) from besoin where gn='$gn' and id not in (select besoin from planning where gn='$gn')";
+			$result1=mysqli_query($db,$sql1)  or die(mysqli_error($db));
+			$sql2="select listPNJ.id as besoin, listPNJ.role as role, listPNJ.login as joueur, (select count(style_pnj.role) from style_pnj inner join style_joueur on style_pnj.style=style_joueur.style where joueur=listPNJ.login and role=listPNJ.role)as stylecommun, (select count(style) from style_pnj where role=listPNJ.role) as stylerole, (select count(style) from style_joueur where joueur=listPNJ.login) as stylejoueur, listPNJ.recur as recur from (select listbesoin.id as id,listbesoin.role as role,inscription.login, listbesoin.recur from inscription, (select besoin.id,besoin.role, debut-prepa as deb, debut+duree as fin, priorite as prio, role.pnj_recurent as recur from event inner join besoin on besoin.event=event.id inner join role on besoin.role=role.id where besoin.id not in (select besoin from planning where gn='$gn') and event.gn='$gn' and role.pnj='1' order by debut-prepa, priorite) as listbesoin where pnj='1' and gn='$gn' and login not in (select planning.joueur from planning inner join inscription as truc on truc.login=planning.joueur where planning.gn='$gn' and ((debut>listbesoin.deb and debut<listbesoin.fin) or (fin>listbesoin.deb and fin<listbesoin.fin) or (debut<listbesoin.deb and fin>listbesoin.fin)))) as listPNJ order by  stylecommun desc,stylerole desc,stylejoueur asc limit 1";
+			$result2=mysqli_query($db,$sql2)  or die(mysqli_error($db));
+
+			while ((mysqli_num_rows($result1) > 0)&&(mysqli_num_rows($result2) > 0)) {
+				$row = mysqli_fetch_assoc($result2);
+echo "result 1a : ".mysqli_num_rows($result1)." result 2a : ".mysqli_num_rows($result2)."<br>";
+				$besoin=$row['besoin'];
+				$perso=$row['joueur'];
+				$recur=$row['recur'];
+				$role=$row['role'];
+// a travailler pour le coté recurrent, parce qu'on peut mettre les informations dans la table "role.login". ne pas oublier de mettre à 0 lors du reset
+				if ($recur==0) {
+					$sql="select besoin.id,debut-prepa as deb,debut+duree as fin from event inner join besoin on event.id=besoin.event  where besoin.id='$besoin' and besoin.gn='$gn' limit 1";
+					$result3=mysqli_query($db,$sql)  or die(mysqli_error($db));
+					$row = mysqli_fetch_assoc($result3);
+					$deb=$row['deb'];
+					$fin=$row['fin'];
+	echo "infos : ".$besoin.",".$perso." ".$deb.",".$fin.",".$gn."<br>";
+					$sql="insert into planning (besoin, joueur, debut, fin, gn) values ('$besoin','$perso','$deb','$fin','$gn')";
+					mysqli_query($db,$sql)  or die(mysqli_error($db));
 				}
+				else {
+					$sql="select id from besoin where role='$role'";
+	echo "recurrence";
+					$result4=mysqli_query($db,$sql)  or die(mysqli_error($db));
+					if ((mysqli_num_rows($result4) > 0)) {
+						while ($row = mysqli_fetch_assoc($result4)) {
+							$besoin2=$row['id'];
+	echo "besoin : ".$besoin2."<br>";
+							$sql="select besoin.id,debut-prepa as deb,debut+duree as fin from event inner join besoin on event.id=besoin.event where besoin.id='$besoin2' and besoin.gn='$gn' limit 1";
+							$result3=mysqli_query($db,$sql)  or die(mysqli_error($db));
+							$row = mysqli_fetch_assoc($result3);
+							$deb=$row['deb'];
+							$fin=$row['fin'];
+			echo "infos : ".$besoin.",".$perso." ".$deb.",".$fin.",".$gn."<br>";
+							$sql="insert into planning (besoin, joueur, debut, fin, gn) values ('$besoin2','$perso','$deb','$fin','$gn')";
+							mysqli_query($db,$sql)  or die(mysqli_error($db));
+						}
+					}
+				}
+				$result1=mysqli_query($db,$sql1)  or die(mysqli_error($db));
+				$result2=mysqli_query($db,$sql2)  or die(mysqli_error($db));
 			}
+echo "result 1 ".mysqli_num_rows($result1)." result 2 ".mysqli_num_rows($result2)."<br>";
+
 		}
 	}
 }

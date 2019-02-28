892eccbe30c68e4b3a2b893faa6160b7d899d5d6
Orion <mathieu.durand@fr.ibm.com>
2019-02-28 17:10:36 +0100

    nettoyage et fin de gestion des updates joueur

--- scenar/joueur-list.php
+++ scenar/joueur-list.php
@@ -22,7 +22,7 @@ else {
     if (mysqli_num_rows($result) > 0) {
     $page=26;
     include 'upper.php';
-    echo "<center>\n";
+    echo "<center>";
     // On vérifie si les champs sont vides
     if($gn==0)
     {
@@ -30,27 +30,33 @@ else {
     }
     // Aucun champ n'est vide, on peut enregistrer dans la table
     else {
-        echo "informations joueur<br>\n";
+		if (isset($_POST['supri'])){
+			$id=$_POST['id'];
+			$sql="delete from inscription where gn='$gn' and login='$id'";
+			mysqli_query($db,$sql)  or die(mysqli_error($db));
+		}
+        echo "informations joueur<br>";
         $sql = "select login_jeu.id,pseudo,login_jeu.nom,prenom,inscription.pnj,inscription.paiement from login_jeu inner join inscription on login_jeu.id=inscription.login where inscription.gn='$gn'";
         $result=mysqli_query($db,$sql)  or die(mysqli_error($db));
-        echo "<table>\n<tr>\n<td>pseudo</td>\n<td>nom</td>\n<td>prenom</td>\n<td>PJ</td>\n<td>paiement</td>\n</tr>\n";
+        echo "<table><tr><td>pseudo</td><td>nom</td><td>prenom</td><td>PJ</td><td>paiement</td><td>operation</td></tr>";
         if (mysqli_num_rows($result)>0) {
             while ($row=mysqli_fetch_assoc($result)) {
-                echo "<tr>\n<td><a href=\"joueur.php?gn=$gn&id=".$row['id']."\">".$row['pseudo']."</a></td>\n";
-                echo "<td>".$row['nom']."</td>\n";
-                echo "<td>".$row['prenom']."</td>\n";
+				$id=$row['id'];
+                echo "<tr><td><a href=\"joueur.php?gn=".$gn."&id=".$id."\">".$row['pseudo']."</a></td>";
+                echo "<td>".$row['nom']."</td>";
+                echo "<td>".$row['prenom']."</td>";
                 echo "<td><input type=checkbox";
                 if ($row['pnj']==0) echo " checked";
-                echo "></td>\n";
+                echo "></td>";
                 echo "<td><input type=checkbox";
                 if ($row['paiement']==1) echo " checked";
-                echo "></td>\n</tr>\n";
+                echo '></td><td><form action="joueur-list.php?gn='.$gn.'" method="post"><input type="hidden" name=id value="'.$id.'"><input type = "submit" name="supri" value = "supprimer"></form></td></tr>';
             }
-            echo "</table>\n";
+            echo "</table>";
         }
 	echo '<form action="add-joueur.php?gn='.$gn.'" method="post">';
     echo '<input type="text" name=nom value="nom a modifier">';
-   echo '<input type = "submit" name="envoi" value = "updater"><br>';
+   echo '<input type = "submit" name="envoi" value = "ajouter"><br>';
     }
     echo '</center></body></html>';
 }   
--- scenar/joueur.php
+++ scenar/joueur.php
@@ -28,24 +28,42 @@ else {
        else 
           $id=0;
     }
-    if (isset($_POST['envoi'])) {
-        if (isset($_POST['paiement'])) {
-            $sql="update inscription set paiement='1' where login='$id' and gn='$gn'";
-            mysqli_query($db,$sql)  or die(mysqli_error($db));
-        }
-        else {
-            $sql="update inscription set paiement='0' where login='$id' and gn='$gn'";
-            mysqli_query($db,$sql)  or die(mysqli_error($db));
-        }
-
-    }
-    $sql = " select admin.medical,admin.admin from admin where admin.gn='$gn' and admin.login='$orga' ";
+	$sql = " select admin.medical,admin.admin from admin where admin.gn='$gn' and admin.login='$orga' ";
     $result=mysqli_query($db,$sql)  or die(mysqli_error($db));
-if (mysqli_num_rows($result) == 1) {
+	if (mysqli_num_rows($result) == 1) {
    $row=mysqli_fetch_assoc($result);
    $doc=$row['medical'];
    $admin=$row['admin'];
 }
+    if (isset($_POST['envoi'])) {
+		$pnj=$_POST['pnj'];
+        if (isset($_POST['paiement'])) {
+            $sql="update inscription set paiement='1',pnj='$pnj' where login='$id' and gn='$gn'";
+            mysqli_query($db,$sql)  or die(mysqli_error($db));
+        }
+        else {
+            $sql="update inscription set paiement='0',pnj='$pnj' where login='$id' and gn='$gn'";
+            mysqli_query($db,$sql)  or die(mysqli_error($db));
+        }
+		if ($admin==1) {
+		$nom=$_POST['nom'];
+		$pseudo=$_POST['pseudo'];
+		$prenom=$_POST['prenom'];
+		$adr=$_POST['adr'];
+		$tel=$_POST['tel'];
+		$cont1=$_POST['cont1'];
+		$cont1t=$_POST['cont1t'];
+		$cont2=$_POST['cont2'];
+		$cont2t=$_POST['cont2t'];
+		$alim=$_POST['alim'];
+		$sante=$_POST['sante'];
+		$email=$_POST['email'];
+		$sql="update login_jeu set email='$email',sante='$sante', nom='$nom', prenom='$prenom', pseudo='$pseudo', adresse='$adr', tel='$tel', contact1_nom='$cont1', contact2_nom='$cont2', contact1_tel='$cont1t', contact2_tel='$cont2t', alim='$alim' where id='$id'";
+		mysqli_query($db,$sql)  or die(mysqli_error($db));
+		}
+	}
+
+
        $page=27;
 include 'upper.php';
 echo '<center>';
@@ -63,7 +81,7 @@ $result=mysqli_query($db,$sql)  or die(mysqli_error($db));
 if (mysqli_num_rows($result) == 1) {
    $row=mysqli_fetch_assoc($result);
    echo '<form action="" method="post">';
-   echo '<input type="text" name=nom value="'.$row['pseudo'].'"><br>';
+   echo '<input type="text" name=pseudo value="'.$row['pseudo'].'"><br>';
    echo "inscrit en tant que ";
    echo '<select name=pnj><option value=0';
 	$pnj=$row['pnj'];
@@ -74,29 +92,36 @@ if (mysqli_num_rows($result) == 1) {
 
    echo '<br>';
    echo '<table>';
-   echo "<tr><td>nom</td><td>".$row['nom']."</td></tr>";
-   echo "<tr><td>prenom</td><td>".$row['prenom']."</td></tr>";
-   echo "<tr><td>adresse</td><td>".$row['adresse']."</td></tr>";
-   echo "<tr><td>telephone</td><td>".$row['tel']."</td></tr>";
+   echo "<tr><td>nom</td><td><input type=text name=nom value=".$row['nom']."></td></tr>";
+   echo "<tr><td>prenom</td><td><input type=text name=prenom value=".$row['prenom']."></td></tr>";
+   echo "<tr><td>email</td><td><input type=text name=email value=".$row['email']."></td></tr>";
+   echo "<tr><td>adresse</td><td><input type=text name=adr value=".$row['adresse']."></td></tr>";
+   echo "<tr><td>telephone</td><td><input type=text name=tel value=".$row['tel']."></td></tr>";
    echo "<tr><td>regime alimentaire</td><td>";
-   switch ($row['alim']) {
-       case 0 : echo 'aucun'; break;
-       case 1 : echo 'allergie'; break;
-       case 2 : echo 'vegetarien'; break;
-   }
+   echo '<select name=alim><option value=0 ';
+   if ($row['alim']==0) echo "selected";
+   echo ">aucun</option>";
+   echo '<option value=1 ';
+	if ($row['alim']==1) echo "selected";
+		 echo ">allergie/intolerange</option>";
+	     echo '<option value=2 ';
+		    if ($row['alim']==2) echo "selected";
+			echo ">vegetarien</option>";
+			echo "</select>";
    echo '</td></tr></table><br>';
    echo 'contacts d urgence<br>';
-   echo "<table><tr><td>".$row['contact1_nom']."</td><td>".$row['contact1_tel']."</td></tr>";
-   echo "<tr><td>".$row['contact2_nom']."</td><td>".$row['contact2_tel']."</td></tr></table><br>";
+   echo "<table><tr><td><input type=text name=cont1 value=".$row['contact1_nom']."></td><td><input type=text name=cont1t value=".$row['contact1_tel']."></td></tr>";
+   echo "<tr><td><input type=text name=cont2 value=".$row['contact2_nom']."></td><td><input type=text name=cont2t value=".$row['contact2_tel']."></td></tr></table><br>";
    if ($doc==1 OR $admin==1) {
        echo 'informations medicales<br>';
-       echo $row['sante']."<br>";
+	   echo "<input type=text name=sante value=".$row['sante']."><br>";
    }
    echo '<br>';
    echo 'paiement recu <input type="checkbox" name=paiement';
       if ($row['paiement']==1)
        echo ' checked';
-   echo '><input type = "submit" name="envoi" value = "updater"><br>';
+   echo '><br>';
+    if ($admin==1) echo '<input type = "submit" name="envoi" value = "updater"><br>';
       }
 
 }

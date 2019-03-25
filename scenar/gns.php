ee2ec1fda41e98d2e39cf6682b2552c728857d2d
Orion <mathieu.durand@fr.ibm.com>
2019-03-25 22:49:26 +0100

    gestion des scenaristes
    et nettoyage de la lsite des GNs

--- scenar/gns.php
+++ scenar/gns.php
@@ -19,7 +19,6 @@ echo '<center>';
 echo 'liste des GN en cours<br>';
 $date=date("Y-m-d H:i").":00";
 $sql="select id,nom from gn where id not in (select gn from admin where admin.login='$id') and debut>'$date' order by debut";
-echo $sql;
 $result=mysqli_query($db,$sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());
 if (mysqli_num_rows($result) > 0) {
     echo '<table><tr><td>nom</td><td>debut</td><td>fin</td><td></td></tr>';
--- scenar/scenar-list.php
+++ scenar/scenar-list.php
@@ -10,7 +10,26 @@ else {
 // On commence par récupérer les champs
    if (isset($_GET['gn'])) 
        $gn=$_GET['gn'];
-   else $gn=0;
+   else 
+		if (isset($_POST['gn']))
+			$gn=$_POST['gn'];
+		else
+			$gn=0;
+   if (isset($_POST['delete']))
+   {
+		$compte=$_POST['id'];
+		$sql="delete from admin where login='$compte' and gn='$gn'";
+		mysqli_query($db,$sql)  or die(mysqli_error($db));
+   }	   
+   if (isset($_POST['update']))
+   {
+	   $compte=$_POST['id'];
+	   $admin=$_POST['admin'];
+	   $doc=$_POST['doc'];
+	   $valid=$_POST['valid'];
+	   $sql="update admin set admin='$admin',medical=$doc,confirmed='$valid' where login='$compte' and gn='$gn'";
+	   mysqli_query($db,$sql)  or die(mysqli_error($db));
+   }
 // On vérifie si les champs sont vides
    $id=$_SESSION['id'];
     $sql="select id from admin where gn='$gn' and login='$id'";
@@ -32,18 +51,22 @@ $result=mysqli_query($db,$sql)  or die(mysqli_error($db));
 echo '<table><tr><td>pseudo</td><td>responsabilite</td><td>admin</td><td>doc</td><td>confirme</td></tr>';
 if (mysqli_num_rows($result) > 0) {
    while($row = mysqli_fetch_assoc($result)) {
-       echo '<tr><td><a href="scenar.php?scenar='. $row["id"]. '&gn='.$gn.'">'. $row["pseudo"].'</a></td>';
+       echo '<tr><form method="POST" action=""><td><a href="scenar.php?scenar='. $row["id"]. '&gn='.$gn.'">'. $row["pseudo"].'</a></td>';
        echo '<td>'.$row['role'].'</td>';
-       echo '<td><input type=checkbox';
+       echo '<td><input type=checkbox name=admin';
        if ($row['admin']==1) echo ' checked'; 
        echo '></td>';
-       echo '<td><input type=checkbox';
+       echo '<td><input type=checkbox name=doc';
       if ($row['medical']==1) echo ' checked';
       echo '></td>';
-      echo '<td><input type=checkbox';
+      echo '<td><input type=checkbox name=valid';
       if ($row['confirmed']==1) echo ' checked';
       echo '></td>';
-      echo '</tr>';
+	  echo '<input type=hidden name=gn value='.$gn.'>';
+	  echo '<td><input type=hidden name=compte value='.$row['id'].'>';
+	  echo '<input type="submit" value="modifier" name="update"></td>';
+	  echo '<td><input type="submit" value="supprimer" name="delete"></td>';
+      echo '</form></tr>';
    }
 }
 echo '</table>';
